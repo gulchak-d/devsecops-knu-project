54470d502de9e518a0d2a4716525998175604f12
gulchak-d <gulchak@knu.ua>
2026-06-03 21:49:00 +0300

    Remove old project structure

--- data_service/app.py
+++
@@ -1,9 +0,0 @@
-import os
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/ping")
-def ping_ip(ip_address: str):
-    result = os.popen(f"ping -c 1 {ip_address}").read() 
-    return {"output": result}
